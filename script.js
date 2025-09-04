0fc2416ccaf48eb116893d369203c13e50d18c7e
raianaBraba <raiana.ribeiro.santos@escola.pr.gov.br>
2025-09-04 10:57:57 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 const reiniciarcriarBtn = document. getElementById('btn-reiniciar');
 
-avanca.forEach(bytton =>
-button.addEventListener('click')
-) 
+avanca.forEach(bytton =>{
+button.addEventListener('click',function(){
+    const atual= document.querySelector('.ativo');
+    const proximoPasso = 'passo-'+this.getAttribute('data-proximo');
+
+    atual.classiList.remove('ativo');
+    const proximoElemento= document.getElementById9(proximoPasso);
+
+    if(proximoElemento{
+        proximoElemento.classiList.add('ativo');
+    } else  {
+        console.error(`elemento com ID "${proximoPasso}" nao encontrado.`);
+    }
+});
+});
