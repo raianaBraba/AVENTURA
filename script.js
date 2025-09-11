e2f11dd5134fbd412f95247fdd0c60c76fbd930c
raianaBraba <raiana.ribeiro.santos@escola.pr.gov.br>
2025-09-11 09:55:28 -0300

    ok

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const avanca = document.querySelectorAll('.btn-proximo');
-const reiniciarcriarBtn = document. getElementById('btn-reiniciar');
+const reiniciarBtn = document.getElementById('btn-reiniciar');
 
-avanca.forEach(bytton =>{
-button.addEventListener('click',function(){
-    const atual= document.querySelector('.ativo');
-    const proximoPasso = 'passo-'+this.getAttribute('data-proximo');
+avanca.forEach(bytton => {
+    button.addEventListener('click', function () {
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-    atual.classiList.remove('ativo');
-    const proximoElemento= document.getElementById9(proximoPasso);
+        atual.classiList.remove('ativo');
+        const proximoElemento = document.getElementById9(proximoPasso);
 
-    if(proximoElemento{
-        proximoElemento.classiList.add('ativo');
-    } else  {
-        console.error(`elemento com ID "${proximoPasso}" nao encontrado.`);
-    }
+        if (proximoElemento){
+            proximoElemento.classiList.add('ativo');
+        } else {
+            console.error(`Elemento com ID "${proximoPasso}" nao encontrado.`);
+        }
+    });
 });
-});
+
+// Reinicia o jogo ao clicar no botão de reiniciando
+if (reiniciarBtn) {
+    reiniciarBtn.addEventListener('click', () => {
+        const atual = document.querySelector('.ativo');
+        atual.classList.remove('ativo');
+        document.getElementById('passo-0').classList.add('ativo');
+
+    });
+
+}
+
+
+
+
